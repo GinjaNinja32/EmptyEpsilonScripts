8695fae8855a6fe755900f23103d8f31af54ebcc
GinjaNinja32 <ginjaninja32@gmail.com>
2024-08-04 18:20:48 +0100

    require: store module return value

--- require.lua
+++ require.lua
@@ -3,13 +3,15 @@
 
 if _LOADED then return end
 _LOADED = {}
+local modules = {}
 local orig_require = require
 function require(lib)
-	if _LOADED[lib] then return end
+	if _LOADED[lib] then return modules[lib] end
 	_LOADED[lib] = true
 
 	if not string.find(lib, ".lua", -4) then
 		lib = lib .. ".lua"
 	end
-	orig_require(lib)
+	modules[lib] = orig_require(lib)
+	return modules[lib]
 end
