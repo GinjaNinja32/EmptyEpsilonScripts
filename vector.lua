dff26180ef2dbfecf953e917f1a323e5e8bc142a
GinjaNinja32 <ginjaninja32@gmail.com>
2024-09-24 20:36:06 +0100

    vector: add vector.distance

--- vector.lua
+++ vector.lua
@@ -41,3 +41,48 @@ end
 function vector.radialDegFromXY(x, y)
 	return math.sqrt(x^2 + y^2), vector.degFromRad(math.atan(y, x))
 end
+
+local function twopoints(name, ...)
+	local args = {...}
+
+	for _, base in ipairs{1, 3} do
+		local x, y
+		if type(args[base]) == "table" and #args[base] == 2 then
+			x, y = table.unpack(args[base])
+		elseif type(args[base]) == "table" or type(args[base]) == "userdata" then
+			x, y = args[base]:getPosition()
+
+			if x == nil then
+				-- TODO: internally docked ships on ECS have no position.
+				-- get the position of the ship they're docked to instead
+				local docked = args[base]:getDockedWith()
+				if docked then
+					x, y = docked:getPosition()
+				end
+			end
+		end
+
+		if x then
+			table.insert(args, base+1, y)
+			args[base] = x
+		end
+	end
+
+	local x1, y1, x2, y2, z = table.unpack(args)
+	if z ~= nil
+		or type(x1) ~= "number"
+		or type(y1) ~= "number"
+		or type(x2) ~= "number"
+		or type(y2) ~= "number"
+		then
+		error("bad args to " .. name .. ": (" .. type(x1) .. ", " .. type(x2) .. ", " .. type(x3) .. ", " .. type(x4) .. ")", 3)
+	end
+
+	return x1, y1, x2, y2
+end
+
+function vector.distance(...)
+	local x1, y1, x2, y2 = twopoints("vector.distance", ...)
+
+	return math.sqrt((x1-x2)^2 + (y1-y2)^2)
+end
